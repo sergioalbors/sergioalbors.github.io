992f1bb7bed00390b3958a04a665395540d9a93d
sergioalbors <serborsss2@gmail.com>
2025-04-18 17:35:55 +0000

    Deploying to gh-pages from @ sergioalbors/sergioalbors.github.io@bf8092a37c9d01199b4099310be9e9a9340b6788 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -41,6 +41,17 @@ ninja.data = [{
             window.location.href = "/blog/2025/gradientdescent/";
           
         },
+      },{id: "post-polynomial-regressions",
+        
+          title: "Polynomial regressions",
+        
+        description: "an example of a blog post with diagrams",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/polynomialregressions/";
+          
+        },
       },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
         
           title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -61,17 +72,6 @@ ninja.data = [{
         handler: () => {
           
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "post-a-post-with-diagrams",
-        
-          title: "a post with diagrams",
-        
-        description: "an example of a blog post with diagrams",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2021/diagrams/";
           
         },
       },{id: "projects-",
