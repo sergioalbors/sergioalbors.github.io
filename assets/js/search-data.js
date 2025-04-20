4de9dcfc39041584e2d31080cf487fb805dfef43
sergioalbors <serborsss2@gmail.com>
2025-04-20 16:34:04 +0000

    Deploying to gh-pages from @ sergioalbors/sergioalbors.github.io@e34640b562d90f03afb31280c548191997fc8862 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "Last updated on April 2025",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-gradient-descent",
         
           title: "GRADIENT DESCENT",
